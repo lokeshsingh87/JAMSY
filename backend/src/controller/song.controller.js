50ed1eacc98b2ab8e94f388ad84b239bc9d43c5a
lokeshsingh87 <116733522+lokeshsingh87@users.noreply.github.com>
2024-12-09 12:49:07 +0530

    Update song.controller.js

--- backend/src/controller/song.controller.js
+++ backend/src/controller/song.controller.js
@@ -11,35 +11,12 @@ export const getAllSongs = async (req, res, next) => {
 	}
 };
 
-export const getFeaturedSongs = async (req, res, next) => {
-	try {
-		// fetch 6 random songs using mongodb's aggregation pipeline
-		const songs = await Song.aggregate([
-			{
-				$sample: { size: 6 },
-			},
-			{
-				$project: {
-					_id: 1,
-					title: 1,
-					artist: 1,
-					imageUrl: 1,
-					audioUrl: 1,
-				},
-			},
-		]);
-
-		res.json(songs);
-	} catch (error) {
-		next(error);
-	}
-};
 
 export const getMadeForYouSongs = async (req, res, next) => {
 	try {
 		const songs = await Song.aggregate([
 			{
-				$sample: { size: 4 },
+				$sample: { size: 1000 },
 			},
 			{
 				$project: {
@@ -58,25 +35,3 @@ export const getMadeForYouSongs = async (req, res, next) => {
 	}
 };
 
-export const getTrendingSongs = async (req, res, next) => {
-	try {
-		const songs = await Song.aggregate([
-			{
-				$sample: { size: 4 },
-			},
-			{
-				$project: {
-					_id: 1,
-					title: 1,
-					artist: 1,
-					imageUrl: 1,
-					audioUrl: 1,
-				},
-			},
-		]);
-
-		res.json(songs);
-	} catch (error) {
-		next(error);
-	}
-};
